b4a8c859ab3251d6e528de0c48403abb70b91c21
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-03 12:51:49 -0300

    Aula  2 de String

---
+++ 4_String_Avancado/scripts.js
@@ -0,0 +1,5 @@
+console.log("Como é seu nome? \nMeu nome é Danilo."); // Pulando linha.
+console.log("O meu nome é 'Danilo'"); // Colocando aspas simples dentro do texto.
+console.log('O nome dele é "Micaela"'); // Colcoando aspas dupla dentro do texto.
+console.log(`A multiplicação de 5 por 3 é de ${5*3}`); // Trazendo uma equação pra dentro da String.
+console.log("O " + "meu " + "nome " + "é " + "Danilo"); //concatenando Strings.
