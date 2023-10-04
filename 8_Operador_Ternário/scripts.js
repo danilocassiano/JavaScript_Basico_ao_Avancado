654c83479be9849b45c7db837c8cf6232b116dd2
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-03 21:29:47 -0300

    Operadores Ternários

---
+++ 8_Operador_Ternário/scripts.js
@@ -0,0 +1,5 @@
+console.log(true ? 1:2);
+console.log(false ? 'falso' : "verdadeiro");
+console.log(5 > 2 ? "É sim" : "É não");
+console.log(15 < 11 ? "Sim" : "Não");
+console.log("Danilo" == "Danilo" ? true : false);
