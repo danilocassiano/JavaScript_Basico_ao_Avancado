18d4b372e883615e4a0ee1c882b172f421ecde56
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-03 21:16:19 -0300

    Operadores Lógicos

---
+++ 7_Operadores_Logicos/scripts.js
@@ -0,0 +1,11 @@
+console.log(5 > 3 && 3 == 2);
+console.log(5 > 3 || 3 == 1);
+console.log();
+console.log(3 == 3 && "Matheus" == "Matheus");
+console.log("Felipe" == "João" || false);
+console.log();
+console.log(!(true || true));
+console.log(!(!(true || true)));
+console.log();
+console.log(true && true);
+console.log(false || false);
