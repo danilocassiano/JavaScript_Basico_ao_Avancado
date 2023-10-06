9dbd7d04ab769937ecc8362340f2336179bd987a
Danilo Cassiano <93688833+danilocassiano@users.noreply.github.com>
2023-10-06 17:44:10 -0300

    Aula de Variaveis e Prompt

---
+++ 10_Prompt/script.js
@@ -0,0 +1,8 @@
+
+let nome = prompt("Qual o seu nome?")
+
+let idade = prompt("Qual a sua idade?");
+
+console.log(nome);
+console.log(idade);
+
---
+++ 9_Variaveis/script.js
@@ -0,0 +1,20 @@
+let nome = "Danilo";
+
+console.log(nome);
+console.log(`meu nome é ${nome}`);
+
+let laranjas = 5;
+
+console.log(laranjas * laranjas);
+
+laranjas = laranjas + 1;
+
+console.log(laranjas);
+
+nome = "Fernando";
+
+console.log(nome);
+
+let um = 1, dois = 2, tres = 3;
+
+console.log( um + dois + tres);
